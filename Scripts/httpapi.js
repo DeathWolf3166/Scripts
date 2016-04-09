8a5e354ecec581b0611e1c8308658a57cc53ad02
BuildTools <max.andreas.schmitt@googlemail.com>
2016-04-09 20:24:04 +0200

    new

---
+++ Scripts/httpapi.js
@@ -0,0 +1,35 @@
+registerPlugin({
+  name: 'http',
+  version: '1.0',
+  description: 'http',
+  author: 'MaxS <info@schmitt-max.com>',
+  vars: {}
+}, function(sinusbot, config) {
+  sinusbot.on('chat', function(ev) {
+    if(ev.msg == '!http') {
+      http = {
+        "method": "GET",
+        "url": "http://sinus.schmitt-max.com/news.php",
+        "timeout": 60000,
+        "headers": [{
+          "Content-Type": "text/plain; charset=UTF-8"
+        }]
+      };
+
+      function Process(error, response) {
+        LastResponse = response
+        if(response.statusCode != 200)
+        {
+          sinusbot.log(error);
+          return;
+        }
+        var output = response.data;
+        var output = output.substr(0, output.length-17);
+        var output = output.slice('133');
+        sinusbot.log(output);
+        chatPrivate(ev.clientId, output)
+      }
+      sinusbot.http(http, Process);
+    }
+  });
+});
