e9a52ab7643fec2d9f774eda9e4797135db62526
BuildTools <max.andreas.schmitt@googlemail.com>
2016-04-11 18:32:02 +0200

    start.js

---
+++ Scripts/start.js
@@ -0,0 +1,11 @@
+registerPlugin({
+    name: 'XXX',
+    version: '0.1',
+    description: 'This a example of a Script Test',
+    author: 'MaxS <info@schmitt-max.com>',
+    vars: {}
+}, function(sinusbot, config) {
+    
+
+  
+});
